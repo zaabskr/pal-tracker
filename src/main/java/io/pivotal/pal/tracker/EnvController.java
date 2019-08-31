3297bf790378a0bd43644e7479c534c54b95fce1
zaabskr <zaabskr@gmail.com>
2019-08-31 08:58:16 +0530

    cloudNativeDeveloperCloudFoundry

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,33 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private Map<String, String> env = new HashMap<>();
+
+    @Autowired
+    public EnvController(@Value("${port:NOT SET}") String port, @Value("${memory.limit:NOT SET}") String memoryLimit,
+                         @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex, @Value("${cf.instance.addr:NOT SET") String cfInstanceAddr)
+    {
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+    }
+
+    @GetMapping("/env")
+    public Map<String, String > getEnv()
+    {
+        return Collections.unmodifiableMap( env);
+    }
+
+}
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,14 +1,24 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String welcomeMessage;
+
+    @Autowired
+    public WelcomeController(@Value("${welcome.message}") String welcomeMessage)
+    {
+        this.welcomeMessage = welcomeMessage;
+    }
+
     @GetMapping("/")
     public String sayHello()
     {
-        return "hello";
+        return welcomeMessage;
     }
 }
