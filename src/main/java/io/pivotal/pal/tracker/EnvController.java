e1cd908ad7f1030c60236c37ebafdbfa88fdb932
zaabskr <zaabskr@gmail.com>
2019-09-01 11:46:24 +0530

    cloudNativeDeveloperDatabaseMigrations env

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -2,17 +2,25 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 @RestController
 public class EnvController {
 
     private Map<String, String> env = new HashMap<>();
+
+    @Autowired
+    private Environment environment;
 
     @Autowired
     public EnvController(@Value("${port:NOT SET}") String port, @Value("${memory.limit:NOT SET}") String memoryLimit,
@@ -28,6 +36,19 @@ public class EnvController {
     public Map<String, String > getEnv()
     {
         return Collections.unmodifiableMap( env);
+    }
+
+    @GetMapping("/env-all")
+    public Map<String, Object > getEnvAll()
+    {
+        Map<String, Object> map = new HashMap();
+        for(Iterator it = ((AbstractEnvironment) environment).getPropertySources().iterator(); it.hasNext(); ) {
+            PropertySource propertySource = (PropertySource) it.next();
+            if (propertySource instanceof MapPropertySource) {
+                map.putAll(((MapPropertySource) propertySource).getSource());
+            }
+        }
+        return map;
     }
 
 }
